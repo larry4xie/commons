2dffa1ce83fcb3cc496bde3cb6d2f1a51b144595
xiegang <xiegang.1988@gmail.com>
2013-04-01 20:45:46 +0800

    JSTL functions encodeURI decodeURI

--- src/main/java/my/commons/web/taglibs/functions/Functions.java
+++ src/main/java/my/commons/web/taglibs/functions/Functions.java
@@ -1,5 +1,9 @@
 package my.commons.web.taglibs.functions;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
 /**
  * JSTL Functions
  * 
@@ -8,5 +12,13 @@ package my.commons.web.taglibs.functions;
  *
  */
 public class Functions {
+	private static String URL_ENCODING = "UTF-8";
 	
+	public static String encodeURI(String s) throws UnsupportedEncodingException {
+		return URLEncoder.encode(s, URL_ENCODING);
+	}
+	
+	public static String decodeURI(String s) throws UnsupportedEncodingException {
+		return URLDecoder.decode(s, URL_ENCODING);
+	}
 }
