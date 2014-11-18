7067b63e1a88069bf0a18b725f0634ab863a6a7d
xiegang <xiegang.1988@gmail.com>
2014-11-18 21:30:43 +0800

    jsp wrapper

---
+++ src/main/java/my/commons/web/JspWrapper.java
@@ -0,0 +1,35 @@
+package my.commons.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+/**
+ * wrapper for jsp template
+ * @author xieg
+ * @since 2014-11-18
+ */
+public class JspWrapper extends HttpServletResponseWrapper {
+	private final StringWriter writer = new StringWriter();
+	
+	public static JspWrapper getInstance(HttpServletResponse response) {
+		return new JspWrapper(response);
+	}
+
+	public JspWrapper(HttpServletResponse response) {
+		super(response);
+	}
+
+	@Override
+	public PrintWriter getWriter() throws IOException {
+		return new PrintWriter(writer);
+	}
+
+	@Override
+	public String toString() {
+		return writer.toString();
+	}
+}
